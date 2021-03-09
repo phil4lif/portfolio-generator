9787cd66ea7196c0f088a7ccdfc3789077df8b62
phil cowan <philipjcowan1@gmail.com>
2021-03-08 18:07:03 -0700

    adds mockdata for testing html template

--- src/page-template.js
+++ src/page-template.js
@@ -1,20 +1,68 @@
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a target="_blank" href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
+const generateAbout = (aboutText) => {
+  if (!aboutText) {
+    return '';
+  }
+  return `
+    <section class="my-3" id="about">
+      <h2>About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `;
 }
 
-module.exports = generatePage;
+module.exports = (templateData) => {
+  console.log(templateData);
+  const { projects, about, ...header } = templateData;
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+  <header>
+    <div class="container flex-row justify-space-between align-center py-3">
+      <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+      <nav class="flex-row">
+        <a target="_blank" class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">
+        GitHub
+        </a>
+      </nav>
+    </div>
+  </header>
+  <main class="container my-5">
+    ${generateAbout(about)}
+  </main>
+  <footer class="container text-center py-3">
+    <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+  </footer>
+</body>
+  </html>
+  `;
+};
+
+// const generatePage = (name, github) => {
+//     return `
+//     <!DOCTYPE html> 
+//     <html lang="en"> 
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <title>Portfolio Demo</title>
+//     </head>
+
+//     <body>
+//       <h1>${name}</h1>
+//       <h2><a target="_blank" href="https://github.com/${github}">Github</a></h2>
+//     </body>
+//     </html>
+//     `;
+// }
+
+// module.exports = generatePage;
